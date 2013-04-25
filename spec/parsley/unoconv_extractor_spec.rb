f64597a35c74d8eb9720f749e75034577ba2a177
Tomas Kramar <kramar.tomas@gmail.com>
2013-04-26 00:52:34 +0200

    Fix failing unoconv test

--- spec/parsley/unoconv_extractor_spec.rb
+++ spec/parsley/unoconv_extractor_spec.rb
@@ -4,6 +4,6 @@ require 'parsley'
 describe Parsley::UnoconvExtractor do
   it 'extracts', slow: true do
     file = File.dirname(__FILE__) + '/fixtures/example.rtf'
-    described_class.extract(file).should == "Hello from RTF."
+    described_class.extract(file).gsub("\u{feff}", "").strip.should == "Hello from RTF."
   end
 end
