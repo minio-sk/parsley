ae625ccc407ca68378a775f388d7ca492fb5eb2d
Tomas Kramar <kramar.tomas@gmail.com>
2014-04-09 10:49:10 +0200

    Fix tests broken by ipv4 resolve mode

--- spec/parsley/curb_downloader_spec.rb
+++ spec/parsley/curb_downloader_spec.rb
@@ -5,7 +5,7 @@ describe Parsley::CurbDownloader do
     headers = double(:headers)
     headers.should_receive(:[]=).with('User-Agent', :something)
 
-    yield_options = double(:options)
+    yield_options = double(:options).as_null_object
     yield_options.should_receive(:headers).and_return(headers)
 
     curl = double.as_null_object
@@ -17,7 +17,7 @@ describe Parsley::CurbDownloader do
   end
 
   it 'passes cookies to curl request' do
-    setup = double(:setup)
+    setup = double(:setup).as_null_object
     setup.should_receive(:cookies=).with('sid=123; abc=321;')
 
     curl = double.as_null_object
@@ -29,7 +29,7 @@ describe Parsley::CurbDownloader do
   end
 
   it 'passes ssl_verify_peer to curl request' do
-    setup = double(:setup)
+    setup = double(:setup).as_null_object
     setup.should_receive(:ssl_verify_peer=).with(false)
 
     curl = double.as_null_object
